61ba506eba8877cc8ec2224cc212e44e7642e60f
Sundze <keyonmwesundze@gmail.com>
2025-07-24 02:50:19 +0100

    ...

---
+++ src/tutorial/01-useState/starter/02-useState-array.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { data } from "../../../../data";
+
+const UseStateArray = () => {
+  const [people, setPeople] = useState(data);
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        // console.log(person);
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default UseStateArray;
