e1661672eba81e49ed5c1f433600c79832db26b4
Sundze <keyonmwesundze@gmail.com>
2025-07-25 03:23:25 +0100

    ...

--- src/tutorial/01-useState/starter/02-useState-array.jsx
+++ src/tutorial/01-useState/starter/02-useState-array.jsx
@@ -4,6 +4,14 @@ import { data } from "../../../../data";
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
 
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
   return (
     <div>
       {people.map((person) => {
@@ -12,9 +20,20 @@ const UseStateArray = () => {
         return (
           <div key={id}>
             <h4>{name}</h4>
+            <button type="button" onClick={() => removeItem(id)}>
+              remove
+            </button>
           </div>
         );
       })}
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        className="btn"
+        onClick={clearAllItems}
+      >
+        clear items
+      </button>
     </div>
   );
 };
