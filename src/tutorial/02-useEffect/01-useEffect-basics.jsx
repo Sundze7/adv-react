2082dd0f20795d591bf9dccd0d4bc0305590b473
Sundze <keyonmwesundze@gmail.com>
2025-08-05 16:01:31 +0100

    useEffect

--- src/tutorial/02-useEffect/01-useEffect-basics.jsx
+++ src/tutorial/02-useEffect/01-useEffect-basics.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
 const UseEffectBasics = () => {
-  return <div>UseEffect-basics</div>;
+  const [value, setValue] = useState(0);
+  const sayHello = () => {
+    console.log("hello there");
+  };
+  sayHello();
+
+  useEffect(() => {
+    console.log("hello from useEffect");
+  }, []);
+  return (
+    <div>
+      <h1>value: {value}</h1>
+      <button type="button" className="btn" onClick={() => setValue(value + 1)}>
+        Click Me
+      </button>
+    </div>
+  );
 };
 export default UseEffectBasics;
