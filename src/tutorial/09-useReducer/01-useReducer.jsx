7709a599aa0c00e7367c9edf960b8b9d723c9969
Sundze <keyonmwesundze@gmail.com>
2025-10-13 12:25:19 +0100

    ...

--- src/tutorial/09-useReducer/01-useReducer.jsx
+++ src/tutorial/09-useReducer/01-useReducer.jsx
@@ -1,27 +1,10 @@
 import { useReducer } from "react";
 import { data } from "../../../data";
 import { CLEAR_LIST, REMOVE_LIST, RESET_LIST } from "./action";
+import reducer from "./reducer";
 
 const defaultState = {
   people: data,
-};
-
-// fxn which controls the app state
-const reducer = (state, action) => {
-  if (action.type === CLEAR_LIST) {
-    return { ...state, people: [] };
-  }
-  if (action.type === RESET_LIST) {
-    return { ...state, people: data };
-  }
-  if (action.type === REMOVE_LIST) {
-    let newPeople = state.people.filter(
-      (person) => person.id !== action.payload.id,
-    );
-    return { ...state, people: newPeople };
-  }
-  //   return state
-  throw new Error(`No matching "${action.type}" - sction type`);
 };
 
 const ReducerBasics = () => {
--- src/tutorial/09-useReducer/reducer.js
+++ src/tutorial/09-useReducer/reducer.js
@@ -1,1 +1,22 @@
 import { CLEAR_LIST, REMOVE_LIST, RESET_LIST } from "./action";
+import { data } from "../../../data";
+
+// fxn which controls the app state
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
+  }
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+  if (action.type === REMOVE_LIST) {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id,
+    );
+    return { ...state, people: newPeople };
+  }
+  //   return state
+  throw new Error(`No matching "${action.type}" - sction type`);
+};
+
+export default reducer;
