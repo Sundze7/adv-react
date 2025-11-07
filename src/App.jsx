e04bb2b6b87ed0e17f2d124a16c28f94e5c4dd65
Sundze <keyonmwesundze@gmail.com>
2025-11-07 14:45:30 +0100

    react redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Starter from "./tutorial/10-performance/01-react-18";
+// import Starter from "./tutorial/10-performance/01-react-18";
 function App() {
   return (
     <div className="container">
-      <Starter />
+      {/* <Starter /> */}
+      <h2>Redux Toolkit</h2>
     </div>
   );
 }
