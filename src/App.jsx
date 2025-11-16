f2e21c3512a05f28ee9a0abd35ada0dc8d034b87
Sundze <keyonmwesundze@gmail.com>
2025-11-16 18:05:27 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import CartContainer from "./reduxFiles/components/CartContainer";
 import NavBar from "./reduxFiles/components/NavBar";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./reduxFiles/features/cart/cartSlice";
 
 // import Starter from "./tutorial/10-performance/01-react-18";
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main className="container">
       {/* <Starter /> */}
