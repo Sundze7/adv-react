1e2ac578bc92626acf78c9265325a33fbf151fcd
Sundze <keyonmwesundze@gmail.com>
2025-11-17 16:57:08 +0100

    ...

--- src/reduxFiles/components/CartContainer.jsx
+++ src/reduxFiles/components/CartContainer.jsx
@@ -1,6 +1,8 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((state) => state.cart);
 
   if (amount < 1) {
@@ -31,7 +33,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
