372c70b93515d68d4135f8af8f5e55dfd2832579
Sundze <keyonmwesundze@gmail.com>
2025-11-27 21:23:12 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,33 @@ import CartContainer from "./reduxFiles/components/CartContainer";
 import NavBar from "./reduxFiles/components/NavBar";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { calculateTotals } from "./reduxFiles/features/cart/cartSlice";
+import {
+  calculateTotals,
+  getCartItems,
+} from "./reduxFiles/features/cart/cartSlice";
 import Modal from "./reduxFiles/components/Modal";
 
 // import Starter from "./tutorial/10-performance/01-react-18";
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main className="container">
