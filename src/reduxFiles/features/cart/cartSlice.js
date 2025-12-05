89a798dac50c8a812a7e3a544a90018690fa414a
Sundze <keyonmwesundze@gmail.com>
2025-12-05 17:23:16 +0100

    ...

--- src/reduxFiles/features/cart/cartSlice.js
+++ src/reduxFiles/features/cart/cartSlice.js
@@ -1,14 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// import cartItems from "../../cartItems"
+import cartItems from "../../cartItems";
 import axios from "axios";
 
 const url = "https://www.course-api.com/react-useReducer-cart-project";
 
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 4,
   total: 0,
-  isLoading: true,
+  isLoading: false,
 };
 
 export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
@@ -55,19 +55,33 @@ const cartSlice = createSlice({
       state.amount = amount;
       state.total = total;
     },
-    extraReducers: {
-      [getCartItems.pending]: (state) => {
-        state.isLoading = true;
-      },
-      [getCartItems.fulfilled]: (state, action) => {
-        console.log(action);
-        state.isLoading = false;
-        state.cartItems = action.payload;
-      },
-      [getCartItems.rejected]: (state) => {
-        state.isLoading = false;
-      },
+    extraReducers: (builder) => {
+      builder
+        .addCase(getCartItems.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(getCartItems.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.cartItems = action.payload;
+        })
+        .addCase(getCartItems.rejected, (state, action) => {
+          console.log(action);
+          state.isLoading = false;
+        });
     },
+    // extraReducers: {
+    //   [getCartItems.pending]: (state) => {
+    //     state.isLoading = true;
+    //   },
+    //   [getCartItems.fulfilled]: (state, action) => {
+    //     console.log(action);
+    //     state.isLoading = false;
+    //     state.cartItems = action.payload;
+    //   },
+    //   [getCartItems.rejected]: (state) => {
+    //     state.isLoading = false;
+    //   },
+    // },
   },
 });
 
