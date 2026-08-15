5ed77cbe18537d0967dde1f75e81f7f03547b952
Sundze <keyonmwesundze@gmail.com>
2026-08-15 14:29:31 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import Starter from "./tutorial/02-useEffect/02-mutiple-effects";
+import Starter from "./tutorial/02-useEffect/03-fetch-data";
+import "./index.css";
+
 function App() {
   return (
     <div className="container">
---
+++ src/tutorial/02-useEffect/03-fetch-data.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
+
+const FetchData = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const users = await response.json();
+        setUsers(users);
+        console.log(users);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <section>
+      <h3>Github Users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+export default FetchData;
